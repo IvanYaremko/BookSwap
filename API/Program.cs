e0e6bcc89be837b23e93d58d0ecf85dbd9bc8083
Ivan <ivan.yaremko@gmail.com>
2021-12-01 12:48:28 +0000

    created initial dotnet classlibs

---
+++ API/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(opt => {
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    // opt.Filters.Add(new AuthorizeFilter(policy));
+})
+    .AddFluitValidation(config => {
+        // config.RegisterValidatorsFromAssemblyContaining<Create>();
+    });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
