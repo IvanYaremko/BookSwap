2d9b739330e28cc593897cab64fc14862ee9d363
Ivan <ivan.yaremko@gmail.com>
2022-01-04 16:04:22 +0000

    added toast notifications for errors from api

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,16 +3,20 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import BookDashboard from '../../features/dashboard/BookDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import BookForm from '../../features/form/BookForm';
 import BookDetails from '../../features/details/BookDetails';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation()
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -20,9 +24,13 @@ function App() {
           <>
             <NavBar />
             <Container style={{ marginTop: '10em' }}>
-              <Route exact path='/books' component={BookDashboard} />
-              <Route path='/books/:id' component={BookDetails} />
-              <Route key={location.key} path={['/createBook', '/edit/:id']} component={BookForm} />
+              <Switch>
+                <Route exact path='/books' component={BookDashboard} />
+                <Route path='/books/:id' component={BookDetails} />
+                <Route key={location.key} path={['/createBook', '/edit/:id']} component={BookForm} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
--- client-app/src/app/stores/Store.ts
+++ client-app/src/app/stores/Store.ts
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
 import BookStore from "./BookStore";
+import CommonStore from "./commonStore";
 
 interface Store {
-    bookStore : BookStore
+    bookStore: BookStore
+    commonStore: CommonStore
 }
 
 export const store : Store = {
-    bookStore: new BookStore()
+    bookStore: new BookStore(),
+    commonStore: new CommonStore()
 }
 
 export const StoreContext = createContext(store)
